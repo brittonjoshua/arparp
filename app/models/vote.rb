3b6035752d43ec741a70723766190c881e2ad26f
Braidy Merkle & Kosha Patel <pair+bmerkle3.kpatel7389@devbootcamp.com>
2017-06-08 15:48:35 -0500

    votes, no view page

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,4 +1,8 @@
 class Vote < ActiveRecord::Base
   belongs_to :voter_id, class_name: :User
   belongs_to :votable, polymorphic: true
+
+  def total_votes
+    self.inject(0) { |sum, vote| sum += vote.value }
+  end
 end
