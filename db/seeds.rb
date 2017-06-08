d2b981b3556122381a302f9eea94d4806b697773
Jack Howard <johncarverhoward@gmail.com>
2017-06-08 16:12:48 -0500

    seed file of funny qs

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+Question.create!(title: "Food?", text: "What do sea lions eat?", best_answer_id: 1)
+
+Question.create!(title: "How Tall?", text: "How tall do sea lions get?", best_answer_id: 1)
+
+Question.create!(title: "Ear Flaps", text: "How many ear flaps do they have?", best_answer_id: 1)
+
+Question.create!(title: "Color", text: "How many ear flaps do they have?", best_answer_id: 1)
+
+Question.create!(title: "Noises so confused", text: "What noises do they make??", best_answer_id: 1)
+
+Question.create!(title: "Fav Time Of Year", text: "What's a sea lion's favorite time of the year?", best_answer_id: 1)
+
+Question.create!(title: "Old Age??", text: "How old do they get?", best_answer_id: 1)
+
+Question.create!(title: "Weight Per Feed", text: "What percentage of their body weight do they eat per feeding?", best_answer_id: 1)
+
+Question.create!(title: "Average Weight", text: "How much do they weigh on average", best_answer_id: 1)
+
