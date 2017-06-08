47ef40e3af1d9fa794357b4bcde69123601ed6c7
Jack Howard <johncarverhoward@gmail.com>
2017-06-08 16:53:48 -0500

    add form to add new question with error handling

--- app/controllers/questions.rb
+++ app/controllers/questions.rb
@@ -7,3 +7,14 @@ get '/questions/:id' do
   @question = Question.find(params[:id])
   erb :'/questions/show'
 end
+
+post '/questions' do
+  @question = Question.new(title: params[:title], text: params[:text], creator: current_user)
+  if @question.save
+    redirect "/questions/#{@question.id}"
+  else
+    @questions = Question.all
+    @errors = @question.errors.full_messages
+    erb :'/questions/index'
+  end
+end
