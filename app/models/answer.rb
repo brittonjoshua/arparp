d2d0312d6a66fe1a5a5936df2d4b7a469282f467
Zach Parsons & Joshua Britton <pair+ZachParsons.brittonjoshua@devbootcamp.com>
2017-06-08 16:19:58 -0500

    added assocations for answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,3 +1,9 @@
 class Answer < ActiveRecord::Base
-  # Remember to create a migration!
+  belongs_to  :question
+  belongs_to :responder, class_name: "User"
+  has_many  :votes, as: :voteable
+  has_many  :comments, as: :commentable
+
+  validates  :text, presence: true
+
 end
