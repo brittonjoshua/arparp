2969028c8fbfb7b77d6275b230908540cd4ce119
Zach Parsons & Joshua Britton <pair+ZachParsons.brittonjoshua@devbootcamp.com>
2017-06-08 18:16:35 -0500

    show message describing which answer is voted best

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -13,7 +13,7 @@ end
 put '/answers/:id' do
   answer = Answer.find(params[:id])
   question = answer.question
-  question.best_answer_id = answer.id
-  # question.update
+  # question.best_answer_id = answer.id
+  question.update_attribute("best_answer_id", answer.id)
   redirect "/questions/#{question.id}"
 end
