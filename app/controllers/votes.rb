7e2826f910330bb4028a925e94b17583861d8eb5
Braidy Merkle & Kosha Patel <pair+bmerkle3.kpatel7389@devbootcamp.com>
2017-06-08 17:12:59 -0500

    add vote buttons for answers and vote route for answers

--- app/controllers/votes.rb
+++ app/controllers/votes.rb
@@ -2,12 +2,12 @@
 
 post 'questions/:question_id/votes' do
   question = Question.find(parms[:question_id])
-  uservote = question.votes.find(session[:user_id])
+  uservote = question.votes.find_by(user_id: session[:user_id])
   @questions = Question.all
 
   if params[:upvote]
     if uservote && uservote.value == 1
-      @errors = ['Aaarp! you have already up voted for this :/']
+      @error = ['Aaarp! you have already up voted for this :/']
       erb :'questions/show'
     elsif uservote && uservote.value == -1
       uservote.update_attributes(value: 1)
@@ -15,21 +15,62 @@ post 'questions/:question_id/votes' do
       redirect "/questions/show"
     else
       vote = question.votes.create(user_id: session[:user_id], value: 1)
-      question.total_votes.to_s
+      question.votes.total_votes.to_s
       redirect "/questions/index"
     end
+
   elsif params[:downvote]
     if uservote && uservote.value = -1
-      @errors = ['Aarp! you can only down vote once fool!!']
+      @error = ['Aarp! you can only down vote once fool!!']
       erb :'questions/show'
     elsif uservote && uservote.value = 1
       uservote.update_attributes(value: -1)
       question.total_votes.to_s
-      redirect "/questions/show"
+      redirect "/questions/#{answer.question_id}"
     else
-      vote = question.votes.create(user_id: session[:user_id], value: 1)
+      vote = question.votes.create(user_id: session[:user_id], value: -1)
       question.total_votes.to_s
       redirect "/questions/index"
     end
   end
 end
+
+post '/answers/:answer_id/votes' do
+  answer = Answer.find_by(id: params[:answer_id])
+  uservote = answer.votes.find_by(user_id: session[:user_id])
+  @question = Question.find_by(id: answer.question_id)
+
+  if params[:upvote]
+    if uservote && uservote.value == 1
+      @error = ['Aaarp! you have already up voted for this :/']
+      erb :'questions/show'
+    elsif uservote && uservote.value == -1
+      uservote.update_attributes(value: 1)
+      answer.total_votes.to_s
+      redirect "/questions/show"
+    else
+      vote = answer.votes.create(user_id: session[:user_id], value: 1)
+      answer.total_votes
+      redirect "/questions/#{answer.question_id}"
+    end
+
+  elsif params[:downvote]
+    if uservote && uservote.value == -1
+      @error = ['Aarp! you can only down vote once fool!!']
+      erb :'questions/show'
+    elsif uservote && uservote.value == 1
+      uservote.update_attributes(value: -1)
+      answer.total_votes.to_s
+      redirect "/questions/#{answer.question_id}"
+    else
+      vote = answer.votes.create(user_id: session[:user_id], value: -1)
+      answer.total_votes.to_s
+      redirect "/questions/#{answer.question_id}"
+    end
+  end
+end
+
+
+
+
+
