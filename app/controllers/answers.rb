72eb775d79684fa679c5c9497bd2d05de81a4ecd
Zach Parsons & Joshua Britton <pair+ZachParsons.brittonjoshua@devbootcamp.com>
2017-06-08 16:56:08 -0500

    update answer form and controller route

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -1,1 +1,10 @@
-#answers.rb
+post '/answers' do
+  answer = Answer.new(params)
+  if answer.save
+    redirect "/questions/#{ answer.question.id }"
+  else
+    status 422
+    @errors= answer.errors.full_messages
+    erb :'questions/show'
+  end
+end
