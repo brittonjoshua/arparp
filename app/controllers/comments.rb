f4a08de1c21109b8bb24bcc82c86adc8489ad6e3
Jack Howard & Zach Parsons <pair+jackhowa.zachparsons@devbootcamp.com>
2017-06-09 15:05:54 -0500

    add a comment on a question only one

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -1,10 +1,10 @@
 post '/questions/:id/comments' do
   @question = Question.find(params[:id])
-  @comment = question.comments.new(text: params[:text], commentor: current_user)
-  if @comment.save?
-
+  @comment = @question.comments.new(text: params[:text], commentor: current_user)
+  if @comment.save
+    redirect "/questions/#{@question.id}"
   else
-
+    @errors = @comment.errors.full_messages
     erb :'/questions/show'
   end
 end
