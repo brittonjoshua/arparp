cb836c8e98d104f32636cc64b64eba7d1bc86070
Joshua Britton <email": null>
2017-06-09 17:22:27 -0500

    add ajax to comment form

--- app/controllers/comments.rb
+++ app/controllers/comments.rb
@@ -3,7 +3,11 @@ post '/questions/:id/comments' do
   @question = Question.find(params[:id])
   @comment = @question.comments.new(text: params[:text], commentor: current_user)
   if @comment.save
-    redirect "/questions/#{@question.id}"
+    if request.xhr?
+      erb :'questions/_new_question_comment', locals: {comment: @comment, question: @question}, layout: false
+    else
+      redirect "/questions/#{@question.id}"
+    end
   else
     @errors = @comment.errors.full_messages
     erb :'/questions/show'
