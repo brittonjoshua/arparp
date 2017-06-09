5fcc25f825938dcf57ac14de7a43b0c4babab4fa
Braidy Merkle <braidymerkle@gmail.com>
2017-06-09 12:20:16 -0500

    ajax successful:

--- public/js/application.js
+++ public/js/application.js
@@ -33,4 +33,25 @@ $(document).ready(function() {
     })
   })
 
+// find form answers are written into
+// prevent default submit
+// find data, action, method
+// make partial so response is only html from the new post
+// after ajax stuff, append the post to the list (ul) of answers
+ $("#add-answer-form").on("submit", function(event){
+    event.preventDefault();
+    var $form = $(this);
+    var $formData = $form.serialize();
+    $.ajax({
+      url: $form.attr('action'),
+      method: $form.attr('method'),
+      data: $formData
+      })
+    .done(function(response){
+    $('ul').append(response);
+    $("#add-answer-form")[0].reset();
+
+    })
+ });
+
 });
